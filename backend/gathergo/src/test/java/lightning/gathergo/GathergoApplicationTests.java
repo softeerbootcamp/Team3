d0e3d28de94cb9191ed84013de0bf6def31a6f94
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-31 11:02:49 +0900

    backend projects init

---
+++ backend/gathergo/src/test/java/lightning/gathergo/GathergoApplicationTests.java
@@ -0,0 +1,13 @@
+package lightning.gathergo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GathergoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
