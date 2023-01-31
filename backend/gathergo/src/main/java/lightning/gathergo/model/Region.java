433a17bcf472a44b2f19da1f4ae4e4385976b694
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-31 12:29:15 +0900

    Region 모델 생성
    Getter함수와 toString 함수 구현
    https://github.com/softeerbootcamp/Team3/issues/1

--- backend/gathergo/src/main/java/lightning/gathergo/model/Region.java
+++ backend/gathergo/src/main/java/lightning/gathergo/model/Region.java
@@ -1,4 +1,24 @@
 package lightning.gathergo.model;
 
+
 public class Region {
+    private Integer id;
+    private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Region{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
