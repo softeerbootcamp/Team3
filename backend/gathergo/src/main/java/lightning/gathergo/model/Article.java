b5f85471d765e05607bd0d94082799428d37503c
sangho0n <tkdgns0917@gmail.com>
2023-01-31 12:39:18 +0900

    feat : 모델 생성
    getter, setter 생성
    https://github.com/softeerbootcamp/Team3/issues/2

---
+++ backend/gathergo/src/main/java/lightning/gathergo/model/Article.java
@@ -0,0 +1,98 @@
+package lightning.gathergo.model;
+
+
+import java.sql.Date;
+
+public class Article {
+    private Long id;
+    private String title;
+    private Long hostId;
+    // private MultipartFile img;
+    private Integer curr;
+    private Integer total;
+    private Boolean isClosed = false;
+    private String content;
+    private Date meetingDay;
+    private Integer regionId;
+    private Integer categoryId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getCurr() {
+        return curr;
+    }
+
+    public void setCurr(Integer curr) {
+        this.curr = curr;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Boolean getClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(Boolean closed) {
+        isClosed = closed;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getMeetingDay() {
+        return meetingDay;
+    }
+
+    public void setMeetingDay(Date meetingDay) {
+        this.meetingDay = meetingDay;
+    }
+
+    public Integer getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(Integer regionId) {
+        this.regionId = regionId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(Long hostId) {
+        this.hostId = hostId;
+    }
+}
