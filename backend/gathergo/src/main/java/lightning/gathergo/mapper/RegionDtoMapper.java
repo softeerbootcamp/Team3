033b572162b1c31747602074edc68062293728ee
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-01 18:52:00 +0900

    List 관련 DTO처리 메서드 추가

--- backend/gathergo/src/main/java/lightning/gathergo/mapper/RegionDtoMapper.java
+++ backend/gathergo/src/main/java/lightning/gathergo/mapper/RegionDtoMapper.java
@@ -4,14 +4,24 @@ import lightning.gathergo.dto.RegionDto;
 import lightning.gathergo.model.Region;
 import org.modelmapper.ModelMapper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RegionDtoMapper {
     private ModelMapper modelMapper;
     //entity to dto
     public RegionDto.Response toRegionResponse(Region region){
         return modelMapper.map(region,RegionDto.Response.class);
     }
+    public List<RegionDto.Response> toRegionResponseList(List<Region> regions){
+        return regions
+                .stream()
+                .map(region -> modelMapper.map(region, RegionDto.Response.class))
+                .collect(Collectors.toList());
+    }
     //dto to entity
-    public Region toRegionResponse(RegionDto.Request regionRequest){
+    public Region toRegion(RegionDto.Request regionRequest){
         return modelMapper.map(regionRequest,Region.class);
     }
 }
