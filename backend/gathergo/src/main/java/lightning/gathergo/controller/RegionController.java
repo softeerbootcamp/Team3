3944e507161c2338bd7d90f051a29c656ccc35c8
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-01 12:37:18 +0900

    conflict resolve

---
+++ backend/gathergo/src/main/java/lightning/gathergo/controller/RegionController.java
@@ -0,0 +1,11 @@
+package lightning.gathergo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/regions",produces = MediaType.APPLICATION_JSON_VALUE)
+public class RegionController {
+
+}
