bf9a48b6b11dacfa340a3fd4268070aae9c26586
sangho0n <tkdgns0917@gmail.com>
2023-02-01 17:38:09 +0900

    refactor : spring data jdbc를 이용하도록 변경
    쿼리를 이용하여 게시물을 넣는 기능 구현

--- backend/gathergo/src/main/java/lightning/gathergo/repository/JdbcArticleRepository.java
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/JdbcArticleRepository.java
@@ -4,121 +4,33 @@ import lightning.gathergo.model.Article;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public class JdbcArticleRepository {
-    private static final Logger logger = LoggerFactory.getLogger(JdbcArticleRepository.class);
+public interface JdbcArticleRepository extends CrudRepository<Article, Long> {
 
-    private final JdbcTemplate jdbcTemplate;
+    @Modifying
+    @Query("INSERT INTO article (title, imgPath, curr, total, isClosed, content, meetingDay) " +
+            " values (:title, :imgPath, :curr, :total, :isClosed, :content, :meetingDay)")
+    public void save(String title, String imgPath, int curr,
+                         int total, boolean isClosed, String content, Date meetingDay);
 
-    @Autowired
-    public JdbcArticleRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    // C
-    public Article addArticle(Article article){
-        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate.getDataSource())
-                .withTableName("articles")
-                .usingGeneratedKeyColumns("id");
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("title", article.getTitle());
-        params.put("hostId", article.getHostId());
-        params.put("curr", article.getCurr());
-        params.put("total", article.getTotal());
-        params.put("isClosed", article.getClosed());
-        params.put("content", article.getContent());
-        params.put("meetingDay", article.getMeetingDay());
-        params.put("regionId", article.getRegionId());
-        params.put("categoryId", article.getCategoryId());
-        Number key = simpleJdbcInsert.executeAndReturnKey(params);
-        article.setId(key.longValue());
-
-        return article;
-    }
-
-    // R
-    public List<Article> findCurrentRegionArticles(int regionId)
-    {
-        String query = "select * from articles where regionId = ?";
-
-        List<Article> result = jdbcTemplate.query(query, articleRowMapper(), regionId);
-        return result;
-    }
-
-    public List<Article> findAllArticles()
-    {
-        String query = "select * from articles";
-
-        return jdbcTemplate.query(query, articleRowMapper());
-    }
-
-    public List<Article> findArticlesByHostId(int hostId)
-    {
-        String query = "select * from articles where hostId = ?";
-
-        List<Article> result = jdbcTemplate.query(query, articleRowMapper(), hostId);
-        return result;
-    }
-
-    public Article findById(Long articleId){
-        String query = "select * from articles where id = ?";
-
-        Article result = jdbcTemplate.queryForObject(query, articleRowMapper(), articleId);
-        return result;
-    }
-
-    // U
-    public Article updateArticle(Article article){
-        String query = "update articles set title = ?, curr = ?, total = ?, isClosed = ?," +
-                "content = ?, meetingDay = ?, regionId = ?, categoryId = ? where id = ?";
-        Long articleId = article.getId();
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("title", article.getTitle());
-        params.put("curr", article.getCurr());
-        params.put("total", article.getTotal());
-        params.put("isClosed", article.getClosed());
-        params.put("content", article.getContent());
-        params.put("meetingDay", article.getMeetingDay().getTime());
-        params.put("regionId", article.getRegionId());
-        params.put("categoryId", article.getCategoryId());
-        jdbcTemplate.update(query, params);
-
-        return findById(article.getId());
-    }
-
-    // D
-    public void deleteArticle(Long articleId){
-        String query = "delete from articles where id = ?";
-        jdbcTemplate.update(query, articleId);
-    }
-
-    private RowMapper<Article> articleRowMapper(){
-        return ((rs, rowNum) -> {
-            Article article = new Article();
-            article.setId(rs.getLong("id"));
-            article.setTitle(rs.getString("title"));
-            article.setHostId(rs.getLong("hostId"));
-            article.setCurr(rs.getInt("curr"));
-            article.setTotal(rs.getInt("total"));
-            article.setClosed(rs.getBoolean("isClosed"));
-            article.setContent(rs.getString("content"));
-            article.setMeetingDay(rs.getDate("meetingDay"));
-            article.setRegionId(rs.getInt("regionId"));
-            article.setCategoryId(rs.getInt("categoryId"));
-            return article;
-        });
+    @Override
+    @Query("SELECT * FROM article WHERE id = :id")
+    default Optional<Article> findById(Long id) {
+        return Optional.empty();
     }
 }
