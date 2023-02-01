03a34c109d76653487ac12fa6ed1d1e3616d9e9b
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-01 12:45:01 +0900

    feat: model mapper를 활용한 매퍼 클래스
    엔티티와 dto 매핑시킴
    https://github.com/softeerbootcamp/Team3/issues/1

--- backend/gathergo/src/main/java/lightning/gathergo/mapper/RegionDtoMapper.java
+++ backend/gathergo/src/main/java/lightning/gathergo/mapper/RegionDtoMapper.java
@@ -1,2 +1,17 @@
-package lightning.gathergo.mapper;public class RegionDtoMapper {
+package lightning.gathergo.mapper;
+
+import lightning.gathergo.dto.RegionDto;
+import lightning.gathergo.model.Region;
+import org.modelmapper.ModelMapper;
+
+public class RegionDtoMapper {
+    private ModelMapper modelMapper;
+    //entity to dto
+    public RegionDto.Response toRegionResponse(Region region){
+        return modelMapper.map(region,RegionDto.Response.class);
+    }
+    //dto to entity
+    public Region toRegionResponse(RegionDto.Request regionRequest){
+        return modelMapper.map(regionRequest,Region.class);
+    }
 }
