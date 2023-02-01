31fd879ce25f4814c48dc6310876874523ecfacb
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-01 12:48:40 +0900

    feat : service 메서드들 추가
    https://github.com/softeerbootcamp/Team3/issues/1

--- backend/gathergo/src/main/java/lightning/gathergo/service/RegionService.java
+++ backend/gathergo/src/main/java/lightning/gathergo/service/RegionService.java
@@ -1,2 +1,34 @@
-package lightning.gathergo.service;public class RegionService {
+package lightning.gathergo.service;
+
+import lightning.gathergo.model.Region;
+import lightning.gathergo.repository.RegionRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class RegionService {
+
+    private final RegionRepository regionRepository;
+
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    public List<Region> getRegions(){
+        return regionRepository.findAll();
+    }
+
+    public Region getRegionById(String id){
+        Integer identifier = Integer.parseInt(id);
+        return regionRepository.findOneById(identifier);
+    }
+
+    public Region getRegionByName(String name){
+        return regionRepository.findOneByName(name);
+    }
+
+
 }
