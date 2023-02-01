7185d7b19ebdddd8937b1fed057038b61e53222a
sangho0n <tkdgns0917@gmail.com>
2023-02-01 22:36:02 +0900

    refactor : Spring Data jdbc를 사용한 save로 변경
    게시물 등록
    게시물 등록 후 id를 얻어내기 위한 메서드 getLastInsertedId 작성
    얻어낸 id를 바탕으로 디비에서 튜플을 얻어내는 findById 작성

--- backend/gathergo/src/main/java/lightning/gathergo/repository/JdbcArticleRepository.java
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/JdbcArticleRepository.java
@@ -1,22 +1,13 @@
 package lightning.gathergo.repository;
 
 import lightning.gathergo.model.Article;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.sql.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -24,13 +15,13 @@ public interface JdbcArticleRepository extends CrudRepository<Article, Long> {
 
     @Modifying
     @Query("INSERT INTO article (title, imgPath, curr, total, isClosed, content, meetingDay) " +
-            " values (:title, :imgPath, :curr, :total, :isClosed, :content, :meetingDay)")
+            "values (:title, :imgPath, :curr, :total, :isClosed, :content, :meetingDay);")
     public void save(String title, String imgPath, int curr,
                          int total, boolean isClosed, String content, Date meetingDay);
 
-    @Override
+    @Query(value = "SELECT id FROM article ORDER BY id DESC LIMIT 1")
+    Long getLastInsertedId();
+
     @Query("SELECT * FROM article WHERE id = :id")
-    default Optional<Article> findById(Long id) {
-        return Optional.empty();
-    }
+    Optional<Article> findById(Long id);
 }
