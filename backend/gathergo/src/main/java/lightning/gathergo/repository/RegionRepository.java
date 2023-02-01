2908316e4ccaf125d559c74d70ca2b2e226768be
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-01 18:52:30 +0900

    쿼리 추가

--- backend/gathergo/src/main/java/lightning/gathergo/repository/RegionRepository.java
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/RegionRepository.java
@@ -1,34 +1,33 @@
 package lightning.gathergo.repository;
 
 import lightning.gathergo.model.Region;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public interface RegionRepository extends CrudRepository<Region, Integer> {
 
+    @Query("select * from region")
     public List<Region> findAll();
+    @Query("select * from region where id = :id ")
     public Optional<Region> findOneById(Integer id);
+    @Query("select * from region where name = :name ")
     public Optional<Region> findOneByName(String name);
-    public Region save(Region region);
-    public void deleteById(Integer id);
+    @Modifying
+    @Query("INSERT INTO region (name) VALUES (:name)")
+    public void save(String name);
+    @Modifying
+    @Query("UPDATE Region SET name = :name WHERE id = :id")
+    void update(Integer id, String name);
+    @Modifying
+    @Query("DELETE FROM Region WHERE id = :id")
+    void deleteById(Integer id);
+    @Modifying
+    @Query("DELETE FROM Region")
     public void deleteAll();
-
-
-
 }
