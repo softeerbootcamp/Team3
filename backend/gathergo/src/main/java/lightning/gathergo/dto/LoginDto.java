5f1c81185454e559628dca997239967f130513c8
YunjaeGong <llegokid@naver.com>
2023-02-02 17:05:49 +0900

    refactor: Login 관련 DTO 리팩토링
    로그인 성공/실패시 반환하던 DTO의 상속관게 제거
    https://github.com/softeerbootcamp/Team3/issues/3

--- backend/gathergo/src/main/java/lightning/gathergo/dto/LoginDto.java
+++ backend/gathergo/src/main/java/lightning/gathergo/dto/LoginDto.java
@@ -21,36 +21,46 @@ public class LoginDto {
             return password;
         }
     }
-    protected static class LoginResponse {
-        protected String message;
-        protected String redirectUrl;
 
-        protected LoginResponse(String message, String redirectUrl) {
-            this.message = message;
-            this.redirectUrl = redirectUrl;
-        }
-    }
-
-    public static class LoginSuccessfulResponse extends LoginResponse {
+    public static class LoginSuccessfulResponse {
+        private String message;
+        private String redirectUrl;
         private Session session;
 
         public LoginSuccessfulResponse(String message, Session s, String redirectUrl) {
-            super(message, redirectUrl);
+            this.message = message;
+            this.redirectUrl = redirectUrl;
             this.session = s;
         }
 
         public Session getSession() {
             return session;
         }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getRedirectUrl() {
+            return redirectUrl;
+        }
     }
 
-    public static class LoginFailedResponse extends LoginResponse {
+    public static class LoginFailedResponse {
+        private String message;
+        private String redirectUrl;
+
         public LoginFailedResponse(String message, String redirectUrl) {
-            super(message, redirectUrl);
+            this.message = message;
+            this.redirectUrl = redirectUrl;
         }
 
         public String getMessage() {
             return message;
+        }
+
+        public String getRedirectUrl() {
+            return redirectUrl;
         }
     }
 }
