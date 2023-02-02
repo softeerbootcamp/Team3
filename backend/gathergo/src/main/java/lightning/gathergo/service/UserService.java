ae54f88bf00a4e2595617b2ebfeffbeb9754a0f8
YunjaeGong <llegokid@naver.com>
2023-02-02 16:24:46 +0900

    feat: 회원가입 메서드 추가
    평문 비밀번호 인코딩 후 DB에 저장
    https://github.com/softeerbootcamp/Team3/issues/3

--- backend/gathergo/src/main/java/lightning/gathergo/service/UserService.java
+++ backend/gathergo/src/main/java/lightning/gathergo/service/UserService.java
@@ -3,6 +3,7 @@ package lightning.gathergo.service;
 import lightning.gathergo.model.User;
 import lightning.gathergo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +12,17 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
     }
 
     public User save(User user) {
