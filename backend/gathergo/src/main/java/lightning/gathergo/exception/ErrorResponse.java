863d400c6645ba0186630ce1261ada8dd933850f
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-02 17:12:06 +0900

    feat : 에러 응답을 내려주는 클래스 구현
    https://github.com/softeerbootcamp/Team3/issues/18

--- backend/gathergo/src/main/java/lightning/gathergo/exception/ErrorResponse.java
+++ backend/gathergo/src/main/java/lightning/gathergo/exception/ErrorResponse.java
@@ -1,2 +1,37 @@
-package lightning.gathergo.exception;public class ErrorResponse {
+package lightning.gathergo.exception;
+
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String code;
+    private String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus().value();
+        this.error = errorCode.getStatus().name();
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(int status, String error, String message) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
