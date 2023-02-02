4fed30be3f2e7823485bf24e86a5de5edc0b43ae
YunjaeGong <llegokid@naver.com>
2023-02-02 16:17:45 +0900

    feat: login api 기본 구현 완성
    비밀번호 암호화, 유저 정보 비교 및 세션 생성
    https://github.com/softeerbootcamp/Team3/issues/3

--- backend/gathergo/src/main/java/lightning/gathergo/controller/AuthController.java
+++ backend/gathergo/src/main/java/lightning/gathergo/controller/AuthController.java
@@ -2,48 +2,55 @@ package lightning.gathergo.controller;
 
 import lightning.gathergo.dto.LoginDto;
 import lightning.gathergo.model.Session;
+import lightning.gathergo.model.User;
 import lightning.gathergo.service.SessionService;
+import lightning.gathergo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class AuthController {
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final SessionService sessionService;
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
     @Autowired
-    public AuthController(SessionService sessionService, PasswordEncoder passwordEncoder) {
+    public AuthController(SessionService sessionService, PasswordEncoder passwordEncoder, UserService userService) {
         this.sessionService = sessionService;
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@RequestBody LoginDto.LoginInput loginDto) {
-        String userId = loginDto.getUserId();
-        String password = loginDto.getPassword();
+        Optional<User> user = userService.findUserByUserId(loginDto.getUserId());
 
-        // TODO: User DB 참조해 userName 가져오기
-        String userName = "";
-        String encodedPassword = "";
+        if (user.isEmpty() || !passwordEncoder.matches(loginDto.getPassword(), user.get().getPassword())) {
+            return new ResponseEntity<>(new LoginDto.LoginFailedResponse("ID나 비밀번호가 일치하지 않습니다", ""), HttpStatus.BAD_REQUEST);
+        }
 
-        // TODO: User DB 참조해 password 비교하기
-        if(!passwordEncoder.matches(password, encodedPassword))
-            return new ResponseEntity<>(new LoginDto.LoginFailedResponse( "ID나 비밀번호가 일치하지 않습니다", ""), HttpStatus.BAD_REQUEST);
+        Session session = sessionService.createSession(user.get().getUserId(), user.get().getUserName());
 
-        Session session = sessionService.createSession(userId, userName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // headers.add("Set-Cookie", "sessionId:" + session.getSessionId());
 
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
-        headers.add("Set-Cookie", session.getSessionId());
-        return new ResponseEntity<>(new LoginDto.LoginSuccessfulResponse( "로그인 성공", session, "/"), headers, HttpStatus.OK);
+        return new ResponseEntity<>(new LoginDto.LoginSuccessfulResponse("로그인 성공", session, "/"), headers, HttpStatus.OK);
     }
 }
