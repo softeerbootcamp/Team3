b2fda7ea14fa943b563eb3012f152a05655cbbab
YunjaeGong <llegokid@naver.com>
2023-02-02 16:22:54 +0900

    feat: User 클래스 Getter/Setter 추가
    https://github.com/softeerbootcamp/Team3/issues/3

--- backend/gathergo/src/main/java/lightning/gathergo/model/User.java
+++ backend/gathergo/src/main/java/lightning/gathergo/model/User.java
@@ -6,15 +6,21 @@ import org.springframework.data.relational.core.mapping.Column;
 public class User {
     @Id
     private Integer id;
+
     private String uuid;
+
     @Column("userid")
     private String userId;
+
     @Column("username")
     private String userName;
 
     private String password;
+
     private String email;
-    private String introduction;
+
+    private String introduction;  // 한 줄 소개
+
     @Column("profilepath")
     private String profilePath;  // 프로필 이미지 경로
 
@@ -26,5 +32,37 @@ public class User {
         this.email = email;
         this.introduction = introduction;
         this.profilePath = profilePath;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public String getProfilePath() {
+        return profilePath;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
