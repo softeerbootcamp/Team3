aa3310a352d2e4406ac854878a9b3ca12b62089e
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2023-02-02 15:35:22 +0900

    [Resolves #1] Region CRUD 구현 (#17)
    * MODEL 생성
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * Region 모델 생성
    
    Getter함수와 toString 함수 구현
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * region model getter setter 추가
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * jdbc를 활용하여 Region CRUD 메서드 구현
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * region repository crud 메서드에 대해 테스트 코드
    
    datasource 의존성 주입하게 해줌
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * cache removed
    
    * gitignore update
    
    * 수정 메서드 테스트 추가
    
    * chore: model mapper 의존성 설정
    
    dto와 entity 간 매핑을 위한 의존성 설정
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * ModelMapper bean으로 등록
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * feat: model mapper를 활용한 매퍼 클래스
    
    엔티티와 dto 매핑시킴
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * feat : dto 생성
    
    입출력은 inner class로 생성
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * feat : service 메서드들 추가
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * feat : jdbc template 에서 spring data jdbc를 사용하도록 수정
    
    repository를 인터페이스로 선언하고 crudRepository를 상속받도록 함
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * test : spring data jdbc를 사용함에 따라 테스트 코드 수정
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * 생성자 추가
    
    * feat : 서비스 메서드들 추가
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * getter setter 추가
    
    * List 관련 DTO처리 메서드 추가
    
    * 쿼리 추가
    
    * 서비스 메서드들 수정
    
    * 리포지토리 테스트 코드 수정
    
    * DTO mapper 테스트 코드
    
    * context loads 삭제
    
    * RegionDTO MAPPER에서 Model Mapper에 대한 의존성을 주입 받도록 수정
    
    테스트하기 이 방식이 더 편해서 이렇게 수정
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * Gson 라이브러리 추가'
    
    * feat : region 컨트롤러 구현
    
    todo : 에러 처리 필요
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * feat : requestDto 분리
    
    생성 요청용, 수정 요청용으로 분리함
    
    * test : Region Mapper test
    
    https://github.com/softeerbootcamp/Team3/issues/1
    
    * 리포지토리 수정
    
    * test : RegionController Test 구현
    
    Mock 객체를 만들고 주입함
    mockMvc를 통해 요청에 대한 응답을 확인함
    https://github.com/softeerbootcamp/Team3/issues/1

--- backend/gathergo/src/main/java/lightning/gathergo/config/AppConfig.java
+++ backend/gathergo/src/main/java/lightning/gathergo/config/AppConfig.java
@@ -11,3 +11,4 @@ public class AppConfig {
         return new ModelMapper();
     }
 }
+
--- backend/gathergo/src/main/java/lightning/gathergo/dto/RegionDto.java
+++ backend/gathergo/src/main/java/lightning/gathergo/dto/RegionDto.java
@@ -6,10 +6,53 @@ public class RegionDto {
         private Integer id;
         private String name;
 
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
-    public static class Request{
+    public static class CreateRequest{
         private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
+    public static class ModifyRequest{
+        private Integer Id;
+        private String name;
+
+        public Integer getId() {
+            return Id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setId(Integer id) {
+            Id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
