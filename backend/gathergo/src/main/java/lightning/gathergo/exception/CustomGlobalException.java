90278ae86a294889dcbe65ef0b4ffeed0620cfbd
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-02 17:12:58 +0900

    feat : 발생되는 전역 에러 객체 생성
    https://github.com/softeerbootcamp/Team3/issues/18

--- backend/gathergo/src/main/java/lightning/gathergo/exception/CustomGlobalException.java
+++ backend/gathergo/src/main/java/lightning/gathergo/exception/CustomGlobalException.java
@@ -1,2 +1,28 @@
-package lightning.gathergo.exception;public class CustomGlobalException {
+package lightning.gathergo.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomGlobalException extends RuntimeException {
+    private final ErrorCode errorCode;
+    private final String message;
+    private final HttpStatus status;
+
+    public CustomGlobalException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+        this.status = errorCode.getStatus();
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
