9b10ecc172f6688a7a3c67aab45b54246bcc98ae
YunjaeGong <llegokid@naver.com>
2023-02-02 18:27:23 +0900

    feat: 회원가입 Dto 추가
    https://github.com/softeerbootcamp/Team3/issues/3

---
+++ backend/gathergo/src/main/java/lightning/gathergo/dto/SignupDto.java
@@ -0,0 +1,67 @@
+package lightning.gathergo.dto;
+
+import lightning.gathergo.model.Session;
+import lightning.gathergo.model.User;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.util.UUID;
+
+public class SignupDto {
+    public static class SignupInput {
+        @Column("userid")
+        private String userId;
+
+        @Column("username")
+        private String userName;
+        private String password;
+        private String email;
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+
+    protected static class SignupResponse {
+        protected String message;
+        protected String redirectUrl;
+
+        protected SignupResponse(String message, String redirectUrl) {
+            this.message = message;
+            this.redirectUrl = redirectUrl;
+        }
+    }
+
+    public static class SignupSuccessfulResponse extends SignupDto.SignupResponse {
+        private Session session;
+
+        public SignupSuccessfulResponse(String message, String redirectUrl) {
+            super(message, redirectUrl);
+        }
+
+        public Session getSession() {
+            return session;
+        }
+    }
+
+    public static class SignupFailedResponse extends SignupDto.SignupResponse {
+        public SignupFailedResponse(String message, String redirectUrl) {
+            super(message, redirectUrl);
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
