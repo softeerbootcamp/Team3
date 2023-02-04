b99927f247c7e40699d6d8d9800e6944607ddc6b
YunjaeGong <llegokid@naver.com>
2023-02-04 18:43:16 +0900

    refactor: SignupDtoMapper 적용
    회원가입 request body 파싱에 SignupDtoMapper 이용해 User로 파싱
    
    https://github.com/softeerbootcamp/Team3/issues/5

--- backend/gathergo/src/main/java/lightning/gathergo/controller/AuthController.java
+++ backend/gathergo/src/main/java/lightning/gathergo/controller/AuthController.java
@@ -2,6 +2,7 @@ package lightning.gathergo.controller;
 
 import lightning.gathergo.dto.LoginDto;
 import lightning.gathergo.dto.SignupDto;
+import lightning.gathergo.mapper.SignupDtoMapper;
 import lightning.gathergo.model.Session;
 import lightning.gathergo.model.User;
 import lightning.gathergo.service.SessionService;
@@ -19,20 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 public class AuthController {
     private final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     private final SessionService sessionService;
     private final PasswordEncoder passwordEncoder;
     private final UserService userService;
+    private final SignupDtoMapper signupDtoMapper;
 
     @Autowired
-    public AuthController(SessionService sessionService, PasswordEncoder passwordEncoder, UserService userService) {
+    public AuthController(SessionService sessionService, PasswordEncoder passwordEncoder, UserService userService, SignupDtoMapper signupDtoMapper) {
         this.sessionService = sessionService;
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
+        this.signupDtoMapper = signupDtoMapper;
     }
 
     @PostMapping("/login")
@@ -47,21 +50,14 @@ public class AuthController {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        // headers.add("Set-Cookie", "sessionId:" + session.getSessionId());
 
+        // TODO: Cookie로 세션 전달, Session mapper 만들기
         return new ResponseEntity<>(new LoginDto.LoginSuccessfulResponse("로그인 성공", session, "/"), headers, HttpStatus.OK);
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody SignupDto.SignupInput signupDto, HttpRequest request) {
-        String userId = signupDto.getUserId();
-        String password = signupDto.getPassword();
-        String email = signupDto.getEmail();
-        String UserName = signupDto.getUserName();
-
-        // TODO: Mapper 이용하기
-
-        User user = new User(UUID.randomUUID().toString(), userId, UserName, password, email, "", "");
+    public ResponseEntity<?> signUp(@RequestBody SignupDto.SignupInput signupDto) {
+        User user = signupDtoMapper.toUser(signupDto);
 
         try {
             userService.addUser(user);
@@ -72,6 +68,7 @@ public class AuthController {
         HttpHeaders headers= new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
         headers.add("Location", "/login");
+
         return new ResponseEntity<>(new SignupDto.SignupSuccessfulResponse( "회원가입 성공", "/login"), headers, HttpStatus.OK);
     }
 
