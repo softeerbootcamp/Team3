7fe09a48d6e9febfdfa9fc5c6bb85edc8ba3fb77
tdmdgh <tdmdgh@gmail.com>
2023-02-05 02:17:06 +0900

    feature: 카테고리 드롭박스 구현 해당 카테고리 썸내일 저장

--- frontend/gathergo/src/Index/index.js
+++ frontend/gathergo/src/Index/index.js
@@ -11,14 +11,23 @@
 //     sidebarCloseEvent();
 //   }
 // });
-//   {
+// function sidebarActiveEvent() {
+//   document.querySelector<HTMLDivElement>('#sidebar')?.classList.add('active');
+//   //TODO: 모든 dropDown 닫아주기
+//   // https://jess2.xyz/css/fade-in-out/
+// }
+// function sidebarCloseEvent() {
+//   document
+//     .querySelector<HTMLDivElement>('#sidebar')
+//     ?.classList.remove('active');
+// }
 function dropDownEvent() {
     document.addEventListener('click', function (e) {
         var target = e.target;
         //다른 화면 터치시 모든 dropDown 닫기 event
         var dropDown = target === null || target === void 0 ? void 0 : target.closest('.nav-item.dropdown');
         if (dropDown === null) {
-            dropDownCloseEvent();
+            dropDownCloseAll();
             return;
         }
         //드롭다운 토글 선택시 열고 닫기 event
@@ -31,12 +40,18 @@ function dropDownEvent() {
             dropDownOpen(dropDown);
         //item 선택
         var dropDownItem = target === null || target === void 0 ? void 0 : target.closest('.dropdown-item');
-        if (dropDownItem) {
-            dropDownToggle.innerHTML = dropDownItem === null || dropDownItem === void 0 ? void 0 : dropDownItem.innerHTML;
+        if (dropDownItem === null)
+            return;
+        if (dropDownItem.classList.contains('category')) {
+            // dropDownToggle.innerHTML = dropDownItem.getAttribute("data-category-title");
+            var index = getElementIndex(dropDownItem);
+            dropDownToggle.innerHTML = category[index + 1];
         }
+        else
+            dropDownToggle.innerHTML = dropDownItem === null || dropDownItem === void 0 ? void 0 : dropDownItem.innerHTML;
     });
 }
-function dropDownCloseEvent() {
+function dropDownCloseAll() {
     document
         .querySelectorAll('.nav-item.dropdown')
         .forEach(function (element) {
@@ -45,6 +60,7 @@ function dropDownCloseEvent() {
     });
 }
 function dropDownOpen(dropDownElement) {
+    dropDownCloseAll();
     var toggleElement = dropDownElement.querySelector('.dropdown-toggle');
     if (toggleElement === null || toggleElement === void 0 ? void 0 : toggleElement.classList.contains('show'))
         return;
@@ -53,26 +69,20 @@ function dropDownOpen(dropDownElement) {
     var menuElement = dropDownElement.querySelector('.dropdown-menu');
     if (menuElement == null)
         return;
-    // menuElement.dataset['bs-popper'] = 'static';
     menuElement.classList.add('show');
     menuElement.setAttribute('data-bs-popper', 'static');
-    // setDataset['bs-popper'] = 'static';
 }
 function dropDownClose(dropDownElement) {
     var toggleElement = dropDownElement.querySelector('.dropdown-toggle');
     if (!(toggleElement === null || toggleElement === void 0 ? void 0 : toggleElement.classList.contains('show')))
         return;
-    toggleElement === null || toggleElement === void 0 ? void 0 : toggleElement.classList.remove('show'); //add()
-    toggleElement === null || toggleElement === void 0 ? void 0 : toggleElement.setAttribute('aria-expanded', 'false'); //true
+    toggleElement === null || toggleElement === void 0 ? void 0 : toggleElement.classList.remove('show');
+    toggleElement === null || toggleElement === void 0 ? void 0 : toggleElement.setAttribute('aria-expanded', 'false');
     var menuElement = dropDownElement.querySelector('.dropdown-menu');
     menuElement === null || menuElement === void 0 ? void 0 : menuElement.classList.remove('show');
-    // delete menuElement?.dataset['bs-popper'];
     menuElement === null || menuElement === void 0 ? void 0 : menuElement.removeAttribute('data-bs-popper');
-    // menuElement?.setAttribute('data-bs-popper', '');
-    // const v = delete menuElement?.dataset['bs-popper'];
-    // console.log(v);
 }
-//header search바
+//header search바 event
 function searchBarEvent() {
     var header = document.querySelector('.header');
     var searchContainer = document.querySelector('.search-container');
@@ -85,24 +95,56 @@ function searchBarEvent() {
         }
     };
 }
-var regionSi = [
-    '서울특별시',
-    '부산광역시',
-    '대구광역시',
-    '인천광역시',
-    '광주광역시',
-    '대전광역시',
-    '울산광역시',
-    '세종특별자치시',
-    '경기도',
-    '강원도',
-    '충청북도',
-    '충청남도',
-    '전라북도',
-    '전라남도',
-    '경상북도',
-    '경상남도',
-    '제주특별자치도',
-];
+var regionSi = {
+    1: '서울특별시',
+    2: '부산광역시',
+    3: '대구광역시',
+    4: '인천광역시',
+    5: '광주광역시',
+    6: '대전광역시',
+    7: '울산광역시',
+    8: '세종특별자치시',
+    9: '경기도',
+    10: '강원도',
+    11: '충청북도',
+    12: '충청남도',
+    13: '전라북도',
+    14: '전라남도',
+    15: '경상북도',
+    16: '경상남도',
+    17: '제주특별자치도',
+    0: '지역을 선택하세요'
+};
+var category = {
+    1: '아웃도어 / 여행',
+    2: '운동 / 스포츠',
+    3: '인문학 / 책/ 글',
+    4: '업종 / 직무',
+    5: '외국 / 언어',
+    6: '문화 / 공연 / 축제',
+    7: '음악 / 악기',
+    8: '패션 / 뷰티',
+    9: '공예 / 만들기',
+    10: '댄스 / 무용',
+    11: '봉사활동',
+    12: '사교 / 인맥',
+    13: '차 / 오토바이',
+    14: '사진 / 영상',
+    15: '야구관람',
+    16: '게임 / 오락',
+    17: '요리 / 제조',
+    18: '반려동물',
+    19: '자유주제',
+    0: '카테고리를 선택하세요'
+};
 searchBarEvent();
 dropDownEvent();
+var getElementIndex = function (element) {
+    var index = 0;
+    var sibling = element.previousElementSibling;
+    while (sibling) {
+        index++;
+        sibling = sibling.previousElementSibling;
+    }
+    return index;
+};
