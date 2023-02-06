7692f4ad150e7ccae7a3429045671924282e48dd
YunjaeGong <llegokid@naver.com>
2023-02-06 16:02:25 +0900

    feat: Transactional 추가
    https://github.com/softeerbootcamp/Team3/issues/5

--- backend/gathergo/src/main/java/lightning/gathergo/service/UserService.java
+++ backend/gathergo/src/main/java/lightning/gathergo/service/UserService.java
@@ -5,11 +5,13 @@ import lightning.gathergo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
