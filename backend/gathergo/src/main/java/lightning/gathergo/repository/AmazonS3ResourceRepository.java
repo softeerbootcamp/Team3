61e36304324068b3eaed3ffdcf05a2ad4447ec62
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2023-02-06 17:45:56 +0900

    Feature/35/image api (#45)
    * chore : s3 의존성 설치
    
    https://github.com/softeerbootcamp/Team3/issues/35
    
    * clear git cache
    
    * com.amazonaws.sdk.disableEc2Metadata 속성을 true로 설정
    
    https://github.com/softeerbootcamp/Team3/issues/35
    
    * feat : 파일 경로 처리 등을 위한 유틸리티
    
    https://github.com/softeerbootcamp/Team3/issues/35
    
    * feat : 해당 이미지 파일에 대한 정보를 담는 DTO
    
    https://github.com/softeerbootcamp/Team3/issues/35
    
    * feat : s3에 이미지 저장하기
    
    1. MultipartFile을 File 객체의 형태로 변환
    2. 위에서 만든 파일객체의 경로와 리네임으로 실제 업로드 하기위해 transferTo() 메서드로 업로드 처리
    3. S3에 파일을 업로드할 때에는 해당 파일(S3에서는 객체)의 권한을 CannedAccessControlList.PublicRead 로 설정해주어야 누구나 파일에 접근이 가능
    
    https://github.com/softeerbootcamp/Team3/issues/35
    
    * 이미지 업로드 관련 컨트롤러 및 서비스
    
    * feat : user.home이 아니라 현재 디렉토리를 받아오도록 수정
    
    https://github.com/softeerbootcamp/Team3/issues/35
    
    * 프로필 이미지명이 유저의 uuid가 되도록 수정

---
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/AmazonS3ResourceRepository.java
@@ -0,0 +1,45 @@
+package lightning.gathergo.repository;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lightning.gathergo.Utils.MultipartUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.sql.SQLOutput;
+
+@Component
+public class AmazonS3ResourceRepository {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    private final AmazonS3Client amazonS3Client;
+
+    @Autowired
+    public AmazonS3ResourceRepository(AmazonS3Client amazonS3Client) {
+        this.amazonS3Client = amazonS3Client;
+    }
+
+    public void save(String fullPath, MultipartFile multipartFile) {
+        //MultipartFile을 File 객체의 형태로 변환
+        File file = new File(MultipartUtil.getLocalCurrentDirectory(), fullPath);
+        try {
+            //위에서 만든 파일객체의 경로와 리네임으로 실제 업로드 하기위해transferTo()메서드로 업로드처리
+            multipartFile.transferTo(file);
+            //S3에 파일을 업로드할 때에는 해당 파일(S3에서는 객체)의 권한을 CannedAccessControlList.PublicRead 로 설정해주어야 누구나 파일에 접근이 가능
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fullPath, file)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        } finally {
+            //로컬(서비스가 구동하는 서버)에서 파일이 복사되어 임시파일과 같이 로컬에 저장이되는데 finally 구문에서 해당 파일을 제거해주도록
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+}
---
+++ backend/gathergo/src/main/java/lightning/gathergo/service/ImageService.java
@@ -0,0 +1,26 @@
+package lightning.gathergo.service;
+
+import lightning.gathergo.dto.ImageDto;
+import lightning.gathergo.repository.AmazonS3ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
+public class ImageService {
+
+    private final AmazonS3ResourceRepository amazonS3ResourceRepository;
+    @Autowired
+    public ImageService(AmazonS3ResourceRepository amazonS3ResourceRepository) {
+        this.amazonS3ResourceRepository = amazonS3ResourceRepository;
+    }
+
+    public ImageDto save(UUID userId, MultipartFile multipartFile) {
+        ImageDto fileDetail = ImageDto.multipartOf(userId,multipartFile);
+        amazonS3ResourceRepository.save(fileDetail.getPath(), multipartFile);
+        return fileDetail;
+    }
+
+}
