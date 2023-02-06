4a30b234d7f496502d269802cd6666cc4693443c
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-06 15:38:16 +0900

    이미지 업로드 관련 컨트롤러 및 서비스

--- backend/gathergo/src/main/java/lightning/gathergo/controller/ImageController.java
+++ backend/gathergo/src/main/java/lightning/gathergo/controller/ImageController.java
@@ -1,2 +1,31 @@
-package lightning.gathergo.controller;public class ImageController {
+package lightning.gathergo.controller;
+
+import lightning.gathergo.dto.ImageDto;
+import lightning.gathergo.service.ImageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@RestController
+@RequestMapping(value = "/image", produces = APPLICATION_JSON_VALUE)
+public class ImageController {
+
+    private final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ImageDto> uploadImage( @RequestPart("file") MultipartFile multipartFile) {
+        ImageDto fileDetail = ImageDto.multipartOf(multipartFile);
+        imageService.save(multipartFile);
+        return ResponseEntity.ok(fileDetail);
+    }
 }
--- backend/gathergo/src/main/java/lightning/gathergo/service/ImageService.java
+++ backend/gathergo/src/main/java/lightning/gathergo/service/ImageService.java
@@ -1,2 +1,24 @@
-package lightning.gathergo.service;public class ImageService {
+package lightning.gathergo.service;
+
+import lightning.gathergo.dto.ImageDto;
+import lightning.gathergo.repository.AmazonS3ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ImageService {
+
+    private final AmazonS3ResourceRepository amazonS3ResourceRepository;
+    @Autowired
+    public ImageService(AmazonS3ResourceRepository amazonS3ResourceRepository) {
+        this.amazonS3ResourceRepository = amazonS3ResourceRepository;
+    }
+
+    public ImageDto save(MultipartFile multipartFile) {
+        ImageDto fileDetail = ImageDto.multipartOf(multipartFile);
+        amazonS3ResourceRepository.save(fileDetail.getPath(), multipartFile);
+        return fileDetail;
+    }
+
 }
