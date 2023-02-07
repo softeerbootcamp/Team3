c5a2cc39bfe299306a1c12e4170d67a1d5f04d81
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-07 14:05:58 +0900

    스케쥴러 동작 서비스 기본 테스트
    https://github.com/softeerbootcamp/Team3/issues/51

--- backend/gathergo/src/main/java/lightning/gathergo/service/SchedulerService.java
+++ backend/gathergo/src/main/java/lightning/gathergo/service/SchedulerService.java
@@ -1,2 +1,16 @@
-package lightning.gathergo.service;public class SchedulerService {
+package lightning.gathergo.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class SchedulerService {
+    @Async
+    @Scheduled(cron="0/60 * * * * ?")
+    public void cronScheduler() {
+        System.out.println(LocalDateTime.now()+" 나는 시스템 시간을 기준으로 1분 마다 주기적으로 실행될거야");
+    }
 }
