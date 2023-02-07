af360834a3b7d1d85f7243a21731302dda432751
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-07 14:01:44 +0900

    feat : 어노테이션 추가
    EnableScheduling : 스케쥴링 활성화
    EnableAsync : 비동기 처리를 위함
    https://github.com/softeerbootcamp/Team3/issues/51

--- backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
+++ backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
@@ -2,7 +2,11 @@ package lightning.gathergo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class GathergoApplication {
 
---
+++ backend/gathergo/src/main/java/lightning/gathergo/config/AsyncConfig.java
@@ -0,0 +1,2 @@
+package lightning.gathergo.config;public class AsyncConfig {
+}
