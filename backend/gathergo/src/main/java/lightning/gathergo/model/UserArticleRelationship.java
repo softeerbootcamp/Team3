4401a8ee055d8f3dac8f4dbbdd30ffe6a206c38d
sangho0n <tkdgns0917@gmail.com>
2023-02-07 17:15:54 +0900

    feat : User - Article N:M Mapping 레포지토리 생성
    https://github.com/softeerbootcamp/Team3/issues/39

---
+++ backend/gathergo/src/main/java/lightning/gathergo/model/UserArticleRelationship.java
@@ -0,0 +1,45 @@
+package lightning.gathergo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("user_article_relationship")
+public class UserArticleRelationship {
+    @Id
+    private Integer id;
+
+    @Column("userid")
+    private Integer userId;
+    @Column("articleid")
+    private Integer articleId;
+
+    public UserArticleRelationship(Integer userId){
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+ }
+
