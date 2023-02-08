8a82f32388d4f99f63ca042b5037fd73450b7a89
YunjaeGong <llegokid@naver.com>
2023-02-08 12:11:03 +0900

    feat: 중복 회원 에러코드 추가
    https://github.com/softeerbootcamp/Team3/issues/44

--- backend/gathergo/src/main/java/lightning/gathergo/exception/ErrorCode.java
+++ backend/gathergo/src/main/java/lightning/gathergo/exception/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
     NO_RESOURCE(HttpStatus.NOT_FOUND,"해당 리소스가 존재하지 않습니다"),
     NAME_DUPLICATED(HttpStatus.CONFLICT,"이미 해당명의 리소스가 존재합니다"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메서드입니다.");
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메서드입니다."),
+    DUPLICATE_KEY(HttpStatus.CONFLICT, "이미 존재하는 식별자입니다.");
 
     private final HttpStatus status;
     private final String message;
