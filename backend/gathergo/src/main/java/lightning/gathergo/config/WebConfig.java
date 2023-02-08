9e07824f67904bd5bb93c33b74dff8c200360c58
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-08 14:53:12 +0900

    cors invalid request resolve

--- backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
+++ backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
@@ -7,15 +7,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*");
+                .addMapping("/**") // 프로그램에서 제공하는 URL
+                .allowedOrigins("*") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
+                .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+                .allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
+                .allowCredentials(false);// 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
+        // .maxAge(1500) // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간 ;
     }
-
 }
 
