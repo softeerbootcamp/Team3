0511e6220f237c993ab202fad33ef12e0fa008da
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-08 11:12:17 +0900

    cors mapping

--- backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
+++ backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("*");
     }
 }
