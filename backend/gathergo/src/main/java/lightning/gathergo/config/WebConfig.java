e80e8d7caf241666789a60fec4373fe36d368912
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-08 14:49:39 +0900

    cors invalid request resolve

--- backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
+++ backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
@@ -1,19 +1,21 @@
 package lightning.gathergo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry
-//                .addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*");
-//    }
-//
-//}
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*");
+    }
+
+}
+
