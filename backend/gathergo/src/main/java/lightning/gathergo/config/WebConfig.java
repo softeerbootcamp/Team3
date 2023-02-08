89aed9a7d55b26029da72b66279711e0aa222980
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-08 10:54:15 +0900

    cors mapping

--- backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
+++ backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
