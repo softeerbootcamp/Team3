9524fbd24b4b3335d71e6e8a26192483386a0b71
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-08 10:43:01 +0900

    cors mapping

--- backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
+++ backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
@@ -2,6 +2,9 @@ package lightning.gathergo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GathergoApplication {
@@ -13,5 +16,16 @@ public class GathergoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GathergoApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://");
+			}
+		};
+	}
+
 
 }
---
+++ backend/gathergo/src/main/java/lightning/gathergo/config/WebConfig.java
@@ -0,0 +1,15 @@
+package lightning.gathergo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://3.36.229.36")
+                .allowedMethods("GET", "POST", "HEAD", "DELETE");
+    }
+}
