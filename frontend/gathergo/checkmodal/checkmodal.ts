4536b4f21ce388fe8c29d441aef4ebfa774986ae
SUNGWHANYOON <ysh0214727@naver.com>
2023-02-09 18:32:17 +0900

    모달의 백그라운드 클릭시 사라짐

--- frontend/gathergo/checkmodal/checkmodal.ts
+++ frontend/gathergo/checkmodal/checkmodal.ts
@@ -2,11 +2,11 @@ const loginCheck = `
     <div class = "loginCheck-modal-container">
         <div class = "loginCheck-modal-background">
             <div class = "loginCheck-modal">
-                <input type="text" value="이 기능은 로그인이 필요합니다." readonly>
-                <input type="text" value="로그인 하시겠습니까?." readonly>
+                <div>이 기능은 로그인이 필요합니다."</div>
+                <div>로그인 하시겠습니까?"<div>
                 <div style = "display : flex; justify-content : space-between">
-                    <button>로그인</button>
                     <button>취소</button>
+                    <button>로그인</button>
                 </div>
             </div>
         </div>
@@ -18,8 +18,8 @@ const joinCheck = `
             <div class = "joinCheck-modal">
                 <input type="text" value="참가 하시겠습니까?." readonly>
                 <div style = "display : flex; justify-content : space-between">
-                    <button>참가</button>
                     <button>취소</button>
+                    <button>참가</button>
                 </div>
             </div>
         </div>
@@ -31,8 +31,8 @@ const gatheringEdit = `
             <div class = "gatheringEdit-modal">
                 <input type="text" value="모임을 수정하시겠습니까?." readonly>
                 <div style = "display : flex; justify-content : space-between">
-                    <button>수정</button>
                     <button>취소</button>
+                    <button>수정</button>
             </div>
         </div>
     </div>
@@ -41,10 +41,10 @@ const gatheringDelete = `
     <div class = "gatheringDelete-modal-container">
         <div class = "gatheringDelete-modal-background">
             <div class = "gatheringDelete-modal">
-                <input type="text" value="모임을 삭제 하시겠습니까?." readonly>
+                <div>모임을 삭제 하시겠습니까?.</div>
                 <div style = "display : flex; justify-content : space-between">
-                    <button>로그인</button>
                     <button>취소</button>
+                    <button>삭제</button>
             </div>
         </div>
     </div>
@@ -54,10 +54,10 @@ const commentDelete = `
     <div class = "commentDelete-modal-container">
         <div class = "commentDelete-modal-background">
             <div class = "commentDelete-modal">
-                <input type="text" value="댓글을 삭제 하시겠습니까?." readonly>
+                <div>댓글을 삭제 하시겠습니까?</div>
                 <div style = "display : flex; justify-content : space-between">
-                    <button>삭제</button>
                     <button>취소</button>
+                    <button>삭제</button>
             </div>
         </div>
     </div>
@@ -86,8 +86,29 @@ function insertCheckModal($checkModal : string){
     }
 
     modalContainer?.appendChild(checkModal);
+    
+    checkModalBackgroundEvent($checkModal,checkModal)
 }
 
 insertCheckModal('commentDelete')
+
+
+function checkModalBackgroundEvent($checkModal : string ,checkModal : HTMLDivElement){ //$checkModal은 그냥 string이고 이름 문자열일 뿐입니다.
+    const checkModalBackgroundName = $checkModal + '-modal-background';
+    const [checkModalBackground] = document.getElementsByClassName(
+      checkModalBackgroundName
+    );
+    checkModalBackground.addEventListener('click', (e) => {
+      if (e.target == checkModalBackground)
+        checkModal.style.display = 'none';
+    });
+}
+
+function commentDeleteEvent(){
+    const commentDeleteButton = document.getElementsByClassName('comment-Delete')[0];
+    commentDeleteButton.addEventListener('click',(e)=>{
+        insertCheckModal('commentDelete');
+    })
+}
 
 export {loginCheck, joinCheck,gatheringDelete,gatheringEdit,commentDelete}
