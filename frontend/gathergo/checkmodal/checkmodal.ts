c5cbeceb54ed083e41c8dd1fef3665811f7932c5
SUNGWHANYOON <ysh0214727@naver.com>
2023-02-09 15:39:22 +0900

    칼럼 삭제 버튼 생성

---
+++ frontend/gathergo/checkmodal/checkmodal.ts
@@ -0,0 +1,43 @@
+const loginCheck = `
+    <div class = "loginCheck-modal-container">
+        <div class = "loginCheck-modal-background">
+            <div class = "loginCheck-modal">
+            </div>
+        </div>
+    </div>
+`;
+const joinCheck = `
+    <div class = "joinCheck-modal-container">
+        <div class = "joinCheck-modal-background">
+            <div class = "joinCheck-modal">
+            </div>
+        </div>
+    </div>
+`;
+const gatheringEdit = `
+    <div class = "gatheringEdit-modal-container">
+        <div class = "gatheringEdit-modal-background">
+            <div class = "gatheringEdit-modal">
+            </div>
+        </div>
+    </div>
+`;
+const gatheringDelete = `
+    <div class = "gatheringDelete-modal-container">
+        <div class = "gatheringDelete-modal-background">
+            <div class = "gatheringDelete-modal">
+            </div>
+        </div>
+    </div>
+`;
+
+const commentDelete = `
+    <div class = "commentDelete-modal-container">
+        <div class = "commentDelete-modal-background">
+            <div class = "commentDelete-modal">
+            </div>
+        </div>
+    </div>
+`;
+
+export {loginCheck, joinCheck,gatheringDelete,gatheringEdit,commentDelete}
