08410e98921c0f8b96ec60728941bfc22df804c6
YunjaeGong <llegokid@naver.com>
2023-02-12 20:14:27 +0900

    feat: Push 알림 발행 테스트 컨트롤러 추가
    https://github.com/softeerbootcamp/Team3/issues/69

--- backend/gathergo/src/main/java/lightning/gathergo/controller/DummyController.java
+++ backend/gathergo/src/main/java/lightning/gathergo/controller/DummyController.java
@@ -1,5 +1,6 @@
 package lightning.gathergo.controller;
 
+import lightning.gathergo.service.FcmMessagingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,9 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class DummyController {
     private static Logger logger = LoggerFactory.getLogger(DummyController.class);
 
+    private final FcmMessagingService fcmMessagingService;
+
+    public DummyController(FcmMessagingService fcmMessagingService) {
+        this.fcmMessagingService = fcmMessagingService;
+    }
+
     @PostMapping("/article/test")
     public ResponseEntity<String> write(@RequestBody String body) {
         logger.debug("/article/test)");
         return new ResponseEntity<>("Authentication Test", HttpStatus.OK);
+    }
+
+    @PostMapping("/devices/{deviceToken}")
+    public String pushNotification(@PathVariable String deviceToken) {
+        // 기기 토큰을 받고 메시지를 발행하는 테스트 컨트롤러
+        System.out.println(deviceToken);
+        String res = fcmMessagingService
+                .sendMessageToToken(deviceToken, "hello", "hello!!");
+        return res;
     }
 }
