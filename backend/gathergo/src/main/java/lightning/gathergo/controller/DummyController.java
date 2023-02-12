3f0a634800dd1e81a66a33cbcbacf2e5e58a414f
YunjaeGong <llegokid@naver.com>
2023-02-12 20:13:41 +0900

    style: 인증 여부 확인할 테스트 컨트롤러 uri 변경
    https://github.com/softeerbootcamp/Team3/issues/69

--- backend/gathergo/src/main/java/lightning/gathergo/controller/DummyController.java
+++ backend/gathergo/src/main/java/lightning/gathergo/controller/DummyController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DummyController {
     private static Logger logger = LoggerFactory.getLogger(DummyController.class);
-    @PostMapping("/api/write")
+
+    @PostMapping("/article/test")
     public ResponseEntity<String> write(@RequestBody String body) {
-        logger.debug("/api/write");
-        return new ResponseEntity<>("write test api", HttpStatus.OK);
+        logger.debug("/article/test)");
+        return new ResponseEntity<>("Authentication Test", HttpStatus.OK);
     }
 }
