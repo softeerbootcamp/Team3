38f6268cee7e694c11fc8139751f9f49b64afa03
YunjaeGong <llegokid@naver.com>
2023-02-13 00:42:24 +0900

    feat: 게시글 (알림)구독 레포지토리 추가
    https://github.com/softeerbootcamp/Team3/issues/69

---
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/NotificationSubscriptionRepository.java
@@ -0,0 +1,28 @@
+package lightning.gathergo.repository;
+
+import lightning.gathergo.model.Subscription;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationSubscriptionRepository extends CrudRepository<Subscription, String> {
+    @Modifying
+    @Query("insert into subscription (articleId, token) VALUES (:articleId, :deviceToken)")
+    void save(@Param("articleId") int articleId, @Param("token") String deviceToken);
+
+    @Query("select articleId, token from subscription")
+    List<Subscription> findAll();
+
+    @Modifying
+    @Query("delete from subscription where articleId = :articleId")
+    void deleteByArticleId(@Param("articleId") int articleId);
+
+    @Modifying
+    @Query("delete from subscription where token = :token")
+    void deleteByDeviceToken(@Param("token") int deviceToken);
+}
