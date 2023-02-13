2623d4639719c9c05c717ba81fc4d0f38457cdb3
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-13 12:46:10 +0900

    add origin

--- backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
+++ backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
@@ -23,7 +23,8 @@ public class GathergoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080","http://localhost:5173");
+						.allowedOrigins("http://localhost:8080","http://localhost:5173")
+						.allowCredentials(true);
 			}
 		};
 	}
