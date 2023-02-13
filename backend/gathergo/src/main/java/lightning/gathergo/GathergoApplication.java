cc850866565d81f97cd7e1e5701794c23acb264c
tdmdgh <76439502+tdmdgh@users.noreply.github.com>
2023-02-13 17:16:54 +0900

    Squashed commit of the following:
    commit 13b9b43d66b5ca3193a10c75fd99c957a803c8dc
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 17:03:50 2023 +0900
    
        roll back
    
    commit b8723944f7edb7290b7dc5222a121c44f59bc4e6
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 16:56:36 2023 +0900
    
         remove bean
    
    commit bb5c0efc785d9b67138f46df9fac0248c6b98f46
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 16:50:05 2023 +0900
    
        allowedMethods 제거
    
    commit 4135b6f9c18b746753cd2b5af5e7e6702d40544c
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 16:44:58 2023 +0900
    
         logout post
    
    commit bb9012e2c5e570102f8975702786fdd3a41b8dee
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 16:43:21 2023 +0900
    
        add allow methods
    
    commit c2f9e6819ea9ae297b67678c7d434d0ab9131b2b
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 16:25:45 2023 +0900
    
         remove origins
    
    commit bc9c9cb267b4001d461fdd2e9ac3e6419d59e050
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 15:27:49 2023 +0900
    
        samesite 옵션
    
    commit 91b5b7dfd0ec17d20fe9ae42dda844d3ca0f36b1
    Author: SUNGWHANYOON <35682200+SUNGWHANYOON@users.noreply.github.com>
    Date:   Mon Feb 13 14:30:02 2023 +0900
    
        Error modal (#85)
    
    commit e67f67c47191ec657debd2a8c6ba69feb3397975
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 13:42:21 2023 +0900
    
         modify origin
    
    commit 2623d4639719c9c05c717ba81fc4d0f38457cdb3
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 12:46:10 2023 +0900
    
         add origin
    
    commit f7143fc6be2b597a3b478fa37d0ab6d7bd86bef4
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 12:42:46 2023 +0900
    
         add origin
    
    commit 6b69a72b9bceb585e1021d1d69143a011fc3d949
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 12:26:10 2023 +0900
    
        redis 추가
    
    commit e4e72d9972c51f2288bd900e930f689dc7792759
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 11:41:48 2023 +0900
    
        origin 추가
    
    commit 9f70dd795fd231f663d56f5ae227fc699e2b25ad
    Author: tdmdgh <76439502+tdmdgh@users.noreply.github.com>
    Date:   Mon Feb 13 11:23:40 2023 +0900
    
        Feature/82/fetches (#84)
    
        * error: main search 디자인 수정
    
        * feat: 로그인 통신 세팅
    
        * feat: 댓글 통신 세팅
    
        * feat: fetch api url 세팅
    
    commit d2af3112066dc764084679f7803b4ba3fe088cd3
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 11:22:43 2023 +0900
    
        origin 추가
    
    commit 3eff277071f281c253eee814438ea84eda5b0e1e
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Mon Feb 13 11:00:39 2023 +0900
    
         set-cookie test
    
    commit 281934cc2a02d4e58ae6dfd8bd21ea20516a008a
    Author: JeongA-Shin <jeonga@khu.ac.kr>
    Date:   Sun Feb 12 17:17:53 2023 +0900
    
        develop branch에 pull_request_template 적용

--- backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
+++ backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
@@ -3,6 +3,7 @@ package lightning.gathergo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,17 +17,5 @@ public class GathergoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GathergoApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*");
-			}
-		};
-	}
-
 
 }
