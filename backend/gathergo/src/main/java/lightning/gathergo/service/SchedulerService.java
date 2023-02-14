4edf245784f31deab41ab3ace3ad4d0cc7f9e5b0
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2023-02-14 16:31:04 +0900

    [Resolves #89] 스케쥴러를 통해 매일 한 번씩 세션 만료 여부 검사하기 (#93)
    * web config 삭제
    
    * feat : 만료된 세션 삭제하는 메서드 구현
    
    https://github.com/softeerbootcamp/Team3/issues/89
    
    * 세션 만료 시간 지정
    
    * feat : enableScheduling 어노테이션 추가
    
    https://github.com/softeerbootcamp/Team3/issues/89
    
    * feat : 현재 세션을 가지고 있는 map의 deep copy를 반환하는 메서드 구현
    
    https://github.com/softeerbootcamp/Team3/issues/89
    
    * feat : cron을 통해 동작 시간 명시
    
    https://github.com/softeerbootcamp/Team3/issues/89
    
    * test : 세션 스케쥴러 테스트 코드
    
    https://github.com/softeerbootcamp/Team3/issues/89

---
+++ backend/gathergo/src/main/java/lightning/gathergo/service/SchedulerService.java
@@ -0,0 +1,55 @@
+package lightning.gathergo.service;
+
+import lightning.gathergo.model.Session;
+import lightning.gathergo.repository.SessionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Component
+public class SchedulerService {
+
+    private final Logger logger = LoggerFactory.getLogger(SchedulerService.class);
+
+    private final SessionRepository repository;
+
+    @Value("${session.duration}")
+    private Integer duration;//=3600;
+
+    @Autowired
+    public SchedulerService(SessionRepository repository) {
+        this.repository = repository;
+    }
+
+    @Scheduled(cron="0 0 02 * * ?") //매일 새벽 2시마다 실행
+    public void session_expiration(){
+        logger.info(LocalDateTime.now().toString()+": session_expiration method");
+        Map<String, Session> sessions =  repository.getCopyOfSessions();
+        for(Map.Entry<String, Session> entry : sessions.entrySet()){
+            String sid = entry.getKey();
+            Session session = entry.getValue();
+
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime created = session.getCreateDate();
+
+            Long age = Duration.between(created, now).getSeconds();
+
+            if(age.intValue() > duration){
+                repository.deleteSessionBySid(sid);
+            }
+        }
+    }
+
+    @Scheduled(cron = "*/10 * * * * *")
+    public void run(){
+        System.out.println("hi");
+    }
+
+}
