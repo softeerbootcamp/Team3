39e8233d04ef46440bba7efada79ffde8585c33b
JeongA-Shin <jeonga@khu.ac.kr>
2023-02-14 16:15:53 +0900

    feat : enableScheduling 어노테이션 추가
    https://github.com/softeerbootcamp/Team3/issues/89

--- backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
+++ backend/gathergo/src/main/java/lightning/gathergo/GathergoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableScheduling
 @SpringBootApplication
 public class GathergoApplication {
 
