e69c44190b44580b823f1a7aef3dee51b736d206
YunjaeGong <llegokid@naver.com>
2023-02-16 10:37:41 +0900

    [Resolves #110] 사용자 프로필 조회 시 비밀번호 반환하지 않도록 (#111)
    * fix: 프로필 조회 json에 비밀번호 필드 삭제
    
    https://github.com/softeerbootcamp/Team3/issues/110
    
    * refactor: 게시글 리스트의 방어적 복사 추가
    
    https://github.com/softeerbootcamp/Team3/issues/110

--- backend/gathergo/src/main/java/lightning/gathergo/dto/UserDto.java
+++ backend/gathergo/src/main/java/lightning/gathergo/dto/UserDto.java
@@ -13,8 +13,6 @@ public class UserDto {
 
         private String userName;
 
-        private String password;
-
         private String email;
 
         private String introduction;
@@ -22,11 +20,10 @@ public class UserDto {
         private List<Article> articleList; //해당 유저가 속한 전체 모임들
         private List<Article> hostingArticleList; // 해당 유저가 호스팅한 모임들
 
-        public ProfileResponse(String uuid, String userId, String userName, String password, String email, String introduction, String profilePath, List<Article> articleList, List<Article> hostingArticleList) {
+        public ProfileResponse(String uuid, String userId, String userName, String email, String introduction, String profilePath, List<Article> articleList, List<Article> hostingArticleList) {
             this.uuid = uuid;
             this.userId = userId;
             this.userName = userName;
-            this.password = password;
             this.email = email;
             this.introduction = introduction;
             this.profilePath = profilePath;
@@ -37,7 +34,7 @@ public class UserDto {
         }
 
         public ProfileResponse(User user, List<Article> articleList, List<Article> hostingArticleList) {
-            this(user.getUuid(), user.getUserId(), user.getUserName(), user.getPassword(), user.getEmail(), user.getIntroduction(), user.getProfilePath(), articleList, hostingArticleList);
+            this(user.getUuid(), user.getUserId(), user.getUserName(), user.getEmail(), user.getIntroduction(), user.getProfilePath(), articleList, hostingArticleList);
         }
 
         public ProfileResponse() {
@@ -65,14 +62,6 @@ public class UserDto {
 
         public void setUserName(String userName) {
             this.userName = userName;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
         }
 
         public String getEmail() {
@@ -104,7 +93,7 @@ public class UserDto {
         }
 
         public void setArticleList(List<Article> articleList) {
-            this.articleList = articleList;
+            this.articleList = List.copyOf(articleList);
         }
 
         public List<Article> getHostingArticleList() {
@@ -112,7 +101,7 @@ public class UserDto {
         }
 
         public void setHostingArticleList(List<Article> hostingArticleList) {
-            this.hostingArticleList = hostingArticleList;
+            this.hostingArticleList = List.copyOf(hostingArticleList);
         }
     }
 
