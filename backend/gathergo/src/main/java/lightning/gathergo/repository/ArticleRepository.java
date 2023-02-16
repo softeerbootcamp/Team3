1a86e9c8bbd15daed1c59fe97b574cc8606a9507
sangho0n <54069713+sangho0n@users.noreply.github.com>
2023-02-16 14:36:27 +0900

    refactor : 쿼리문에 order by 각각 추가
    https://github.com/softeerbootcamp/Team3/issues/115

--- backend/gathergo/src/main/java/lightning/gathergo/repository/ArticleRepository.java
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/ArticleRepository.java
@@ -37,7 +37,7 @@ public interface ArticleRepository extends CrudRepository<Article, Integer> {
     @Query("insert into article (hostId, title, total, isClosed, content, meetingDay, location, regionId, categoryId, uuid) " +
             "values (:hostId, :title, :total, :isClosed, :content, :meetingDay, :location, :regionId, :categoryId, :uuid);")
     public void save(Integer hostId, String title,
-                         int total, boolean isClosed, String content, Timestamp meetingDay, String location, int regionId, int categoryId, String uuid);
+                     int total, boolean isClosed, String content, Timestamp meetingDay, String location, int regionId, int categoryId, String uuid);
 
     @Query(value = "select id from article order by id desc LIMIT 1")
     Integer getLastInsertedId();
@@ -54,12 +54,12 @@ public interface ArticleRepository extends CrudRepository<Article, Integer> {
     @Query("select * from article")
     List<Article> findAllArticles();
 
-    @Query("select * from article where regionId = :regionId")
+    @Query("select * from article where regionId = :regionId order by meetingDay")
     List<Article> findCurrentRegionArticles(int regionId);
-    @Query("select * from article where categoryId = :categoryId")
+    @Query("select * from article where categoryId = :categoryId order by meetingDay")
     List<Article> findArticlesByCategoryId(int categoryId);
 
-    @Query("select * from article where regionId = :regionId and categoryId = :categoryId")
+    @Query("select * from article where regionId = :regionId and categoryId = :categoryId order by meetingDay")
     List<Article> findArticlesByRegionAndCategory(int regionId, int categoryId);
 
 
@@ -79,25 +79,26 @@ public interface ArticleRepository extends CrudRepository<Article, Integer> {
     public void deleteById(Integer id);
 
     // 게시물에 달린 댓글 조회
-    @Query("select * from comment c join article a on c.articleId = a.id where a.uuid = :uuid")
+    @Query("select * from comment c join article a on c.articleId = a.id where a.uuid = :uuid order by c.date desc")
     List<Comment> findCommentsByArticleUuid(String uuid);
 
     // 검색어만 주어진 경우
-    @Query("select a.* from article a where title like CONCAT('%', :keyword, '%')")
+    @Query("select a.* from article a where title like CONCAT('%', :keyword, '%') order by meetingDay")
     List<Article> findByKeyword(String keyword);
 
     // 검색어와 지역이 주어진 경우
-    @Query("select a.* from article a where regionId=:regionId and title like CONCAT('%', :keyword, '%')")
+    @Query("select a.* from article a where regionId=:regionId and title like CONCAT('%', :keyword, '%') order by meetingDay")
     List<Article> findByKeywordAndRegion(String keyword, Integer regionId);
 
     // 검색어와 카테고리가 주어진 경우
-    @Query("select a.* from article a where categoryId=:categoryId and title like CONCAT('%', :keyword, '%')")
+    @Query("select a.* from article a where categoryId=:categoryId and title like CONCAT('%', :keyword, '%') order by meetingDay")
     List<Article> findByKeywordAndCategory(String keyword, Integer categoryId);
 
     // 검색어와 카테고리, 지역 모두 주어진 경우
-    @Query("select a.* from article a where regionId=:regionId and categoryId=:categoryId and title like CONCAT('%', :keyword, '%')")
+    @Query("select a.* from article a where regionId=:regionId and categoryId=:categoryId and title like CONCAT('%', :keyword, '%') order by meetingDay")
     List<Article> findByKeywordAndRegionAndCategory(String keyword, Integer regionId, Integer categoryId);
 
     @Query("select u.* from user u join article a on u.id = a.hostId where a.uuid=:articleUuid")
     User findUserInfoByArticleHostId(String articleUuid);
 }
+
