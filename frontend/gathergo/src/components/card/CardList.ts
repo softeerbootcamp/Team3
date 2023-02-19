512629f9f7e5b3daad7e48b4a4a554bfabf72613
tdmdgh <76439502+tdmdgh@users.noreply.github.com>
2023-02-20 03:16:52 +0900

    feat: 메인페이지 탭 구현

--- frontend/gathergo/src/components/card/CardList.ts
+++ frontend/gathergo/src/components/card/CardList.ts
@@ -7,11 +7,13 @@ class CardList {
   cardsState: Tcard[];
   element: HTMLDivElement;
   navigate: Navigate;
-  constructor(navigate:Navigate) {
+  type:string;
+  constructor(navigate:Navigate,type:string) {
     this.cardsState = store.getState().cards;
     this.element = document.createElement('div');
     this.element.classList.add('card-wrapper');
     this.navigate = navigate;
+    this.type = type;
     this.render();
     store.subscribe(() => {
       const newState = store.getState().cards;
@@ -27,7 +29,11 @@ class CardList {
   }
   generateCards() {
     const state = store.getState();
-    state.cards.forEach((cardData: Tcard) => {
+    let cardDatas = state.cards;
+    if(this.type==='RECENT'){
+      cardDatas = cardDatas.sort((a:Tcard, b:Tcard) => a.meetingDay.getTime() - b.meetingDay.getTime() );
+    }
+      cardDatas.forEach((cardData: Tcard) => {
       const card = new Card(cardData);
       this.element.appendChild(card.element);
       card.element.addEventListener('click', async () => {
--- frontend/gathergo/src/pages/home.ts
+++ frontend/gathergo/src/pages/home.ts
@@ -1,11 +1,12 @@
-
 import { fetchCardDetail, getArticles } from '../common/Fetches';
 import Navigate from '../common/utils/navigate';
 import CardList from '../components/card/CardList';
 import CardModal from '../components/card/CardModal';
 import HeaderHome from '../components/header/HeaderHome';
+import TabHome from '../components/tabs/tabHome';
+import TabContent from '../components/tabs/tabContent';
 import store from '../store/store';
-import Fba from '../components/fba/fba'
+import Fba from '../components/fba/fba';
 class Home {
   $container: HTMLElement | null;
   navigate: Navigate;
@@ -20,21 +21,28 @@ class Home {
     const headerHome = new HeaderHome();
     this.$container.appendChild(headerHome.element);
 
-    // console.log(window.location.href);
-    store.dispatch(await getArticles(store.getState().filters))
-    const cardList = new CardList(this.navigate);
-    this.$container.appendChild(cardList.element);
+
+    store.dispatch(await getArticles(store.getState().filters));
+    const cardList = new CardList(this.navigate,'NEW');
+    // this.$container.appendChild(cardList.element);
+    const cardList2 = new CardList(this.navigate,'RECENT');
+    // this.$container.appendChild(cardList2.element);
+    const tabContent = new TabContent(cardList.element,cardList2.element);
+    // this.$container.appendChild(tabContent.element);
+
+    const homeTab = new TabHome(tabContent);
+    this.$container.appendChild(homeTab.element);
+    this.$container.appendChild(homeTab.tabContent.element);
 
     const modal = new CardModal();
     this.$container.appendChild(modal.element);
     this.closeModalEvent(modal.element);
 
-    
     const queryString = new URLSearchParams(window.location.search);
     const feed = queryString.get('feed');
     if (feed) {
       cardList.openCardModal();
-      store.dispatch( await fetchCardDetail(feed));
+      store.dispatch(await fetchCardDetail(feed));
     }
 
     this.matchSearchBarValue();
@@ -52,7 +60,7 @@ class Home {
 
     window.addEventListener('popstate', () => {
       // if(!modalEle.classList.contains('out'))
-        this.closeModal(modalEle);
+      this.closeModal(modalEle);
     });
   }
 
@@ -60,57 +68,60 @@ class Home {
     modalContainer.classList.add('out');
     document.body?.removeAttribute('class');
     // this.navigate.to('/')
-    history.replaceState(store.getState(), "", '/');
-
+    history.replaceState(store.getState(), '', '/');
   }
 
-  matchSearchBarValue(){
-    const categorySidebar = document.querySelector('.keyword-input') as HTMLInputElement;
-    const hideSidebar = document.querySelector('.keyword-input-sticky') as HTMLInputElement;
-    
-    categorySidebar?.addEventListener('keyup',(e)=>{
-      
+  matchSearchBarValue() {
+    const categorySidebar = document.querySelector(
+      '.keyword-input'
+    ) as HTMLInputElement;
+    const hideSidebar = document.querySelector(
+      '.keyword-input-sticky'
+    ) as HTMLInputElement;
+
+    categorySidebar?.addEventListener('keyup', (e) => {
       hideSidebar.value = categorySidebar.value;
-      if(e.key==='Enter') this.keywordSearch();
-    })
-    hideSidebar?.addEventListener('keyup',(e)=>{
+      if (e.key === 'Enter') this.keywordSearch();
+    });
+    hideSidebar?.addEventListener('keyup', (e) => {
       categorySidebar.value = hideSidebar.value;
-      if(e.key==='Enter') this.keywordSearch();
-    })
-  }
-  keywordSearchEvent(){
-    
-    const searchBtns = document.querySelectorAll('.keyword-search-btn');
-    searchBtns.forEach((btn) => {
-        btn.addEventListener('click',()=>{
-          this.keywordSearch();
-        })
+      if (e.key === 'Enter') this.keywordSearch();
     });
   }
-  async keywordSearch(){
-    let keywrodValue = document.querySelector<HTMLInputElement>('.keyword-input')?.value;
-    if( !keywrodValue) keywrodValue=""
-    store.dispatch(await getArticles({...store.getState().filters, keyword:keywrodValue}))
-
-    console.log('ljlkjlk')
+  keywordSearchEvent() {
+    const searchBtns = document.querySelectorAll('.keyword-search-btn');
+    searchBtns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        this.keywordSearch();
+      });
+    });
   }
-  setFBA(){
-    new Fba(this.$container)
+  async keywordSearch() {
+    let keywrodValue =
+      document.querySelector<HTMLInputElement>('.keyword-input')?.value;
+    if (!keywrodValue) keywrodValue = '';
+    store.dispatch(
+      await getArticles({ ...store.getState().filters, keyword: keywrodValue })
+    );
   }
-  openSidebarEvent(){
+  setFBA() {
+    new Fba(this.$container);
+  }
+  openSidebarEvent() {
     const alarm = document.querySelector('#sidebar-tigger');
     console.log(alarm);
-    
-    document.querySelector<HTMLElement>('#sidebar-tigger')?.addEventListener('click',()=>{
-      this.openSidebar();
-    })
-}
-openSidebar(){
+
+    document
+      .querySelector<HTMLElement>('#sidebar-tigger')
+      ?.addEventListener('click', () => {
+        this.openSidebar();
+      });
+  }
+  openSidebar() {
     document.querySelector('#alarm-sidebar')?.classList.add('active');
     document.querySelector('.sidebar-container')?.classList.add('active');
-    document.body.classList.add('modal-active')
-    
-}
+    document.body.classList.add('modal-active');
+  }
 }
 
 export default Home;
