af4e2b36e943ac31be47f4ed348ba514bbf42912
YunjaeGong <llegokid@naver.com>
2023-02-20 15:31:28 +0900

    fix: Repository 엔티티 변경
    https://github.com/softeerbootcamp/Team3/issues/167

--- backend/gathergo/src/main/java/lightning/gathergo/repository/NotificationRepository.java
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/NotificationRepository.java
@@ -1,6 +1,6 @@
 package lightning.gathergo.repository;
 
-import lightning.gathergo.model.Subscription;
+import lightning.gathergo.model.Notification;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface NotificationRepository extends CrudRepository<Subscription, String> {
+public interface NotificationRepository extends CrudRepository<Notification, String> {
     @Modifying
     @Query("insert into notification (articleUuid, title, body,  issueDateTime) VALUES (:articleUuid, :title, :body, now())")
     void save(@Param("articleUuid") String articleUuid, @Param("title") String title, @Param("body") String body);
 
-    @Query("select n.articleUuid, n.title, n.body, a.meetingDay, n.issueDateTime from notification as n join article a on n.articleUuid = a.uuid")
-    List<Subscription> findAll();
+    @Query("select articleUuid, title, body, issueDateTime from notification where articleUuid=articleUuid")
+    List<Notification> findNotificationByArticleId(@Param("articleUuid") String articleUuid);
 
     @Modifying
     @Query("delete from notification where articleUuid = :articleUuid")
