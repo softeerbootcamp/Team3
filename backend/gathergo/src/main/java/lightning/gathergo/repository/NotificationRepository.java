1d511d26bf8fe67a9d4794be087bb9fe6290c7a6
YunjaeGong <llegokid@naver.com>
2023-02-20 17:55:08 +0900

    feat: 알림 레코드 반환 레포지토리 추가
    https://github.com/softeerbootcamp/Team3/issues/167

--- backend/gathergo/src/main/java/lightning/gathergo/repository/NotificationRepository.java
+++ backend/gathergo/src/main/java/lightning/gathergo/repository/NotificationRepository.java
@@ -7,19 +7,31 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface NotificationRepository extends CrudRepository<Notification, String> {
     @Modifying
-    @Query("insert into notification (articleUuid, title, body,  issueDateTime) VALUES (:articleUuid, :title, :body, now())")
+    @Query("insert into notification (articleUuid, title, body, issueDateTime) VALUES (:articleUuid, :title, :body, now())")
     void save(@Param("articleUuid") String articleUuid, @Param("title") String title, @Param("body") String body);
 
-    @Query("select articleUuid, title, body, issueDateTime from notification where articleUuid=articleUuid")
-    List<Notification> findNotificationByArticleId(@Param("articleUuid") String articleUuid);
+    @Modifying
+    @Query("insert into notification (articleUuid, title, body, issueDateTime) VALUES (:articleUuid, :title, :body, :issueDateTime)")
+    void save(@Param("articleUuid") String articleUuid, @Param("title") String title, @Param("body") String body, @Param("issueDateTime") Timestamp issueDateTime);
+
+    @Query("select articleUuid, title, body, issueDateTime from notification where articleUuid=:articleUuid")
+    List<Notification> findByArticleId(@Param("articleUuid") String articleUuid);
+
+    @Query("select articleUuid, title, body, issueDateTime from notification where articleUuid in (:articleUuid)")
+    List<Notification> findByArticleIds(@Param("articleUuid") List<String> articleUuid);
 
     @Modifying
     @Query("delete from notification where articleUuid = :articleUuid")
     int deleteByArticleUuid(@Param("articleUuid") String articleUuid);
+
+    @Modifying
+    @Query("delete from notification where articleUuid in (:articleUuid)")
+    int deleteByArticleUuids(@Param("articleUuid") List<String> articleUuid);
 
 }
