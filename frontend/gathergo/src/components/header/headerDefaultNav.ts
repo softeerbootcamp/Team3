423e7171edff148b36b4d832dc358cc9146a0ac4
tdmdgh <76439502+tdmdgh@users.noreply.github.com>
2023-02-21 19:31:54 +0900

    feat: home화면 로그아웃 (#196)

--- frontend/gathergo/src/components/header/headerDefaultNav.ts
+++ frontend/gathergo/src/components/header/headerDefaultNav.ts
@@ -13,18 +13,18 @@ class HeaderDefaultNav {
   genertateNav(type: string) {
     switch (type) {
       case 'post':
-        return `
+        return `<li class="nav-item profile-icon">
+                <a class="nav-link" href="/profile" data-hover="내 프로필">
+                  <span>내 프로필</span>
+                </a>
+              </li>
+              <li class="nav-item divider"></li>
               <li class="nav-item">
                 <a class="nav-link cancel" href="/" data-hover="취소">
                   <span>취소</span>
                 </a>
               </li>
-              <li class="nav-item divider"></li>
-              <li class="nav-item profile-icon">
-                <a class="nav-link" href="/profile" data-hover="내 프로필">
-                  <span>내 프로필</span>
-                </a>
-              </li>`;
+              `;
       case 'login':
         return `<li class="nav-item">
         <a class="nav-link cancel" href="/" data-hover="취소">
@@ -33,17 +33,19 @@ class HeaderDefaultNav {
     </li>
       `;
       case 'profile':
-        return `<li class="nav-item">
+        return `
+        <li class="nav-item">
+              <a class="nav-link" href="./post" data-hover="모임 만들기">
+                <span>모임 만들기</span>
+              </a>
+            </li>
+      <li class="nav-item divider"></li>
+      <li class="nav-item">
         <a class="nav-link cancel" href="/" data-hover="취소">
           <span>취소</span>
         </a>
       </li>
-      <li class="nav-item divider"></li>
-      <li class="nav-item">
-              <a class="nav-link" href="./post" data-hover="모임 만들기">
-                <span>모임 만들기</span>
-              </a>
-            </li>`;
+      `;
       default:
         return '';
     }
