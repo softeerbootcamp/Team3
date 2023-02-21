9c7681356b8c275e1df46ffe0001a9d75dd1bc62
SUNGWHANYOON <35682200+SUNGWHANYOON@users.noreply.github.com>
2023-02-21 11:49:15 +0900

    Alarm onmessage (#183)
    * Readme conflict 해결
    
    * firebase fetch 붙임:
    
    * articleuuid->useruuid
    
    * onMessage 안지운 상태, firebase Fetch 완료
    
    * onMessage 지움

--- frontend/gathergo/src/components/fba/chatBotOpenAI.js
+++ frontend/gathergo/src/components/fba/chatBotOpenAI.js
@@ -1,22 +1,21 @@
-
 // import { Configuration, OpenAIApi }from 'openai';
-import config from '../../key'
-  export async function generateResponse(userInput) {
-// eslint-disable-next-line no-undef
-const apiKey = config.openAIapikey;//'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';
+import config from '../../key';
+export async function generateResponse(userInput) {
+  // eslint-disable-next-line no-undef
+  const apiKey = config.openAIapikey; //'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';
 
-// const prompt = 'Your prompt goes here';
+  // const prompt = 'Your prompt goes here';
 
-// const configuration = new Configuration({
-//     apiKey: apiKey,
-//   });
-//   const openai = new OpenAIApi(configuration);
+  // const configuration = new Configuration({
+  //     apiKey: apiKey,
+  //   });
+  //   const openai = new OpenAIApi(configuration);
 
-const model = 'davinci';
-const prompt = 'Please answer my question about my website.';
-const maxTokens = 150;
-const temperature = 0.5;
-const data = `
+  const model = 'davinci';
+  const prompt = 'Please answer my question about my website.';
+  const maxTokens = 150;
+  const temperature = 0.5;
+  const data = `
 The name of this website is GatherGo. GatherGo is a region-based instant meeting platform. When people exercise, study, travel, lack of people, or just want to meet someone for no reason, use GatherGo! In GatherGo, you can participate in meetings to meet friends.
 Users can use this website while join the meeting or hosting. Furthermore, they can communicate with comments.
 Users can find the meeting they want by selecting regional filters, category filters, and inputting keywords.
@@ -43,45 +42,44 @@ If the user wants to make friends, Then GatherGo is the best choice.
 If the user wants to host meeting, you can give them the procedures: "first, login or signup please. Second, click the button "모임 만들기". Third, fill the blanks. Last, enjoy your journey."
 `;
 
+  const fullPrompt = `${prompt}\n\nUser: ${userInput}\n\nAI:`;
 
-    const fullPrompt = `${prompt}\n\nUser: ${userInput}\n\nAI:`;
-  
-    // Combine the prompt with the training data
-    const trainingData = `${data}\n\n${fullPrompt}`;
-  
-      
-      const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-      };
-      
-      const bodydata = {
-        prompt: trainingData,
-        temperature: temperature,
-        max_tokens: maxTokens,
-        n: 1,
-        stop: '\n',
-        model: model,
-      };
-      
-      const requestOptions = {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(bodydata),
-      };
-      
-      const response = await fetch('https://api.openai.com/v1/completions', requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-          //console.log(data.choices[0].text);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-      console.log(response)
-    // Extract and return the response text
-    return response.choices[0].text.trim();
-  }
-  
+  // Combine the prompt with the training data
+  const trainingData = `${data}\n\n${fullPrompt}`;
 
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${apiKey}`,
+  };
+
+  const bodydata = {
+    prompt: trainingData,
+    temperature: temperature,
+    max_tokens: maxTokens,
+    n: 1,
+    stop: '\n',
+    model: model,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(bodydata),
+  };
+
+  const response = await fetch(
+    'https://api.openai.com/v1/completions',
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+      //console.log(data.choices[0].text);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  console.log(response);
+  // Extract and return the response text
+  return response.choices[0].text.trim();
+}
